be3d5dcba08f4eb950129cf20b834e0709f9d411
adantas- <adantas-@student.42sp.org.br>
2023-11-21 16:54:46 -0300

    +M: update main to be equal to the PDF

--- Mod02/ex02/source/main.cpp
+++ Mod02/ex02/source/main.cpp
@@ -3,7 +3,7 @@
  * @author adantas- (adantas-@student.42sp.org.br)
  * @brief Main file.
  * @version 1
- * @date 2023-11-20
+ * @date 2023-11-21
  * 
  * @copyright Copyright (c) 2023
  */
@@ -11,37 +11,19 @@
 #include "Fixed.class.hpp"
 #include <iostream>
 
-namespace tests {
-	void test1(void) {
-		const Fixed e(-42);
-		const Fixed f(e);
-		Fixed g;
-
-		g = Fixed(-3.141f);
-		std::cout << e << std::endl;
-		std::cout << f << std::endl;
-		std::cout << g << std::endl;
-	}
-}
-
-int main(void) {
+int main( void ) {
 	Fixed a;
-	Fixed const b(10);
-	Fixed const c(42.42f);
-	Fixed const d(b);
-	
-	a = Fixed(1234.4321f);
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-	tests::test1();
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
 	return 0;
 }
