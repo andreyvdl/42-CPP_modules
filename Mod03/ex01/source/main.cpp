d74135b88b11e20a89beaef27b6b60c480450b39
adantas- <adantas-@student.42sp.org.br>
2023-11-29 16:48:38 -0300

    M: removed old main content and changed makefile target name

--- Mod03/ex01/source/main.cpp
+++ Mod03/ex01/source/main.cpp
@@ -13,49 +13,9 @@
 #include "ClapTrap.class.hpp"
 
 namespace tests {
-	void turnAction(ClapTrap& attacker, ClapTrap& adversary) {
-		switch (rand() % 2) {
-			case 0:
-				attacker.attack(adversary.getName());
-				adversary.takeDamage(attacker.getDamage());
-				break;
-			case 1:
-				attacker.beRepaired(rand() % 10);
-		}
-	}
 
-	void fight(ClapTrap& claptrapA, ClapTrap& claptrapB) {
-		srand(time(NULL));
-		while ((claptrapA.getHitPoints() > 0 && claptrapB.getHitPoints() > 0)
-			|| (claptrapA.getEnergyPoints() > 0
-				&& claptrapB.getEnergyPoints() > 0)
-		)
-			switch (rand() % 2) {
-				case 0:
-					turnAction(claptrapA, claptrapB);
-					break;
-				case 1:
-					turnAction(claptrapB, claptrapA);
-			}
-	}
 }
 
 int main(void) {
-	ClapTrap claptrap("clap trap");
-	ClapTrap cl4p_tp;
-	ClapTrap cl4p_tp2(claptrap);
 
-	claptrap.setDamage(2);
-	cl4p_tp.setDamage(3);
-	cl4p_tp2.setDamage(1);
-	cl4p_tp2.setName("Scrap trap");
-	tests::fight(cl4p_tp, cl4p_tp2);
-	claptrap.setHitPoints(10);
-	cl4p_tp2.setHitPoints(10);
-	std::cout << std::endl;
-	tests::fight(claptrap, cl4p_tp2);
-	claptrap.setHitPoints(10);
-	cl4p_tp.setHitPoints(10);
-	std::cout << std::endl;
-	tests::fight(claptrap, cl4p_tp);
 }
