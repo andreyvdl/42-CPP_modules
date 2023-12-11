2e36052a75c555ce5e609b5b49bda335b67be946
adantas- <adantas-@student.42sp.org.br>
2023-12-11 15:03:32 -0300

    +: added class to finish implementation

---
+++ Mod04/ex03/include/AMateria.abstract.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file AMateria.abstract.hpp
+ * @author adantas- (adantas-@student.42sp.org.br)
+ * @brief Definition of the AMateria class.
+ * @version 1
+ * @date 2023-12-11
+ * 
+ * @copyright Copyright (c) 2023
+ */
+
+#pragma once
+#ifndef AMATERIA_ABSTRACT_HPP
+# define AMATERIA_ABSTRACT_HPP
+# include <iostream>
+
+class AMateria
+{
+	protected:
+		// [...]
+
+	public:
+		AMateria(void);
+		AMateria(AMateria const& that);
+		~AMateria(void);
+		AMateria& operator=(AMateria const& that);
+
+		AMateria(std::string const& type);
+		// [...]
+
+		std::string const& getType(void) const; //Returns the materia type
+
+		virtual AMateria* clone(void) const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
