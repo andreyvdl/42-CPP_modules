c2956b65f4ce7320a130520c454c3037cee1850b
adantas- <adantas-@student.42sp.org.br>
2023-12-12 11:10:35 -0300

    +: added methods for AMateria

---
+++ Mod04/ex03/source/AMateria.abstract.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file AMateria.abstract.cpp
+ * @author adantas- (adantas-@student.42sp.org.br)
+ * @brief Methods for the AMateria class.
+ * @version 1
+ * @date 2023-12-12
+ * 
+ * @copyright Copyright (c) 2023
+ */
+
+#include <iostream>
+#include "AMateria.abstract.hpp"
+#include "ICharacter.interface.hpp"
+
+/* TRUCTORS ================================================================= */
+
+AMateria::AMateria(void): _type("none")
+{
+	std::cout << "AMateria default constructor called!" << std::endl;
+}
+
+AMateria::AMateria(std::string const& type): _type(type)
+{
+	std::cout << "AMateria type constructor called!" << std::endl;
+}
+
+AMateria::AMateria(AMateria const& that)
+{
+	std::cout << "AMateria copy constructor called!" << std::endl;
+	*this = that;
+}
+
+AMateria::~AMateria(void)
+{
+	std::cout << "AMateria destructor called!" << std::endl;
+}
+
+/* OPERATORS ================================================================ */
+
+AMateria& AMateria::operator=(AMateria const& that)
+{
+	std::cout << "AMateria assignation operator called!" << std::endl;
+	if (this != &that) {
+		this->_type = that._type;
+	}
+	return (*this);
+}
+
+/* METHODS ================================================================== */
+
+std::string const& AMateria::getType(void) const
+{
+	return (this->_type);
+}
+
+void AMateria::use(ICharacter& target)
+{
+	std::cout << "* uses materia on " << target.getName() << " *" << std::endl;
+}
