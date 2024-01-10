fccda8cab71768f4146a677208aedc03baca9691
adantas- <adantas-@student.42sp.org.br>
2024-01-10 14:22:30 -0300

    +: Form class declaration

---
+++ Mod05/ex01/include/Form.class.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef FORM_CLASS_HPP
+# define FORM_CLASS_HPP
+
+# include <iostream>
+# include "Grades.exception.hpp"
+# include "Bureaucrat.class.hpp"
+
+class Form
+{
+	private:
+		const std::string	_name;
+		const int			_signGrade;
+		const int			_execGrade;
+		bool				_signed;
+
+	public:
+				Form(void);
+				Form(std::string const name, int signGrade, int execGrade)
+					throw(GradeTooHighException, GradeTooLowException);
+				Form(Form const& that);
+				~Form(void);
+		Form&	operator=(Form const& that);
+
+		const std::string	getName(void) const;
+		int					getSignGrade(void) const;
+		int					getExecGrade(void) const;
+		bool				getSigned(void) const;
+		void				beSigned(Bureaucrat const& that)
+			throw(GradeTooLowException);
+};
+
+std::ostream& operator<<(std::ostream& oStream, const Form& that);
+
+#endif
