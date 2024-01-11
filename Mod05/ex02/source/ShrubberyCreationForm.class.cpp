ef835b384972b687dbbeaf2d4fdfa0b4a43a9742
adantas- <adantas-@student.42sp.org.br>
2024-01-11 17:32:59 -0300

    +: implementation of the tree form

---
+++ Mod05/ex02/source/ShrubberyCreationForm.class.cpp
@@ -0,0 +1,72 @@
+#include "../include/ShrubberyCreationForm.class.hpp"
+#include <fstream>
+
+#define TREES "\
+       _-_               _-_               _-_\n\
+    /~~   ~~\\         /~~   ~~\\         /~~   ~~\\\n\
+ /~~         ~~\\   /~~         ~~\\   /~~         ~~\\\n\
+{               } {               } {               }\n\
+ \\  _-     -_  /   \\  _-     -_  /   \\  _-     -_  /\n\
+   ~  \\ //  ~       ~  \\ //  ~       ~  \\ //  ~\n\
+_- -   | | _- _   _- -   | | _- _   _- -   | | _- _\n\
+  _ -  | |   -_     _ -  | |   -_     _ -  | |   -_\n\
+      // \\             // \\             // \\"
+
+/* TRUCTORS ================================================================= */
+
+ShrubberyCreationForm::ShrubberyCreationForm(void):
+AForm("ShrubberyCreationForm", 145, 137), _target("NoTarget") {};
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string const target):
+AForm("ShrubberyCreationForm", 145, 137), _target(target) {};
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const& that)
+{
+	*this = that;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(void) {};
+
+/* OPERATORS ================================================================ */
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(
+	ShrubberyCreationForm const& that
+)
+{
+	if (this != &that) {
+		AForm::operator=(that);
+		const_cast<std::string&>(_target) = that._target;
+	}
+	return (*this);
+}
+
+/* METHODS ================================================================== */
+
+void ShrubberyCreationForm::execute(Bureaucrat const& executor) const
+throw(GradeTooLowException, NotSignedException)
+{
+	if (!getSigned()) {
+		throw NotSignedException();
+	} else if (executor.getGrade() > getExecGrade()) {
+		throw GradeTooLowException();
+	} else {
+		drawTree(_target + "_shrubbery");
+	}
+}
+
+void drawTree(std::string const& fileName)
+{
+	std::ofstream out(fileName.c_str(), std::ios::out | std::ios::trunc);
+
+	if (!out.is_open() || out.fail()) {
+		throw std::ios_base::failure("Error opening file");
+	}
+	out << TREES;
+	if (out.fail()) {
+		throw std::ios_base::failure("Error writing to file");
+	}
+	out.close();
+	if (out.fail()) {
+		throw std::ios_base::failure("Error closing file");
+	}
+}
